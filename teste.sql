34b87aa2fd536d0158639009dd1c7583138c2a74
TiagoMabango <tiagomabango@gmail.com>
2022-09-08 11:13:35 +0100

    Base de dados para teste

---
+++ teste.sql
@@ -0,0 +1,21 @@
+create database teste
+
+use teste
+
+create table empployes(
+	ID smallint primary key identity,
+	FirstName varchar(50),
+	LastName varchar(50),
+	Created_at datetime default current_timestamp,
+	updated_at datetime
+)
+
+create table tasks(
+	ID smallint primary key identity,
+	task varchar(60),
+	empployee_fk  smallint,
+	constraint empployee_fk foreign key(empployee_fk)
+	references empployes (ID) on delete cascade,
+	Created_at datetime default current_timestamp,
+	updated_at datetime
+)
